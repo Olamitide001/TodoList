4a91a7a7b21349ed6acac8139d84dbea6b82c6f6
olamitide_ <azeezaolad@gmail.com>
2023-03-29 12:56:41 +0100

    This is the first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+const todoInput =document.querySelector('.todo-input')
+const todoButton = document.querySelector('.todo-button')
+const todoContainer = document.querySelector('.todo-container')
+const filterOption = document.querySelector('.filter')
+const deletebutton = document.querySelector('.delete')
+// const completebutton = document.querySelector('.mark')
+
+todoContainer.innerHTML=" "
+// todoButton.addEventListener('click', addTodo)
+todoButton.addEventListener('click' , addTodo)
+function addTodo(event){
+    event.preventDefault()
+    const newTodo = document.createElement('div')
+    newTodo.classList.add('item')
+    todoContainer.appendChild(newTodo)
+    const paragraph= document.createElement('p')
+    paragraph.classList.add('item-list')
+    paragraph.innerText = todoInput.value
+    newTodo.appendChild(paragraph)
+    const mark = document.createElement('button')
+    mark.classList.add('mark')
+    newTodo.appendChild(mark)
+    const trash = document.createElement('button')
+    trash.classList.add('delete')
+    newTodo.appendChild(trash)
+    const check = document.createElement('p')
+    check.classList.add('square')
+    mark.appendChild(check)
+    const remove = document.createElement('p')
+    remove.classList.add('circle')
+    trash.appendChild(remove)
+    todoInput.value = ""
+
+mark.addEventListener('click', function(){
+    newTodo.style.textDecoration = "line-through"
+    newTodo.style.background ="grey"
+})
+mark.addEventListener('dblclick', function(){
+    newTodo.style.textDecoration = "none"
+    newTodo.style.background ="white"
+})
+trash.addEventListener('click', function(){
+    newTodo.style.display = "none"
+})
+}
